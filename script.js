e3a6c514fc33ab0347bed8679450a2b28635d13a
vini642 <valentimvini09@gmail.com>
2025-11-22 12:44:38 -0300

    Refactor clik function and remove test function
    Removed the test function and updated the clik function to redirect to the about page.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function clik() {
+  window.location.replace("https://vini642.github.io/about.html")
   alert("this is my website!");
   alert("its just made for tests and practices for HTML and JavaScript and abit of CSS");
   alert("hope you enjoy!");
-}
-function test() {
-  window.location.replace("https://vini642.github.io/about.html")
 }
